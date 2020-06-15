bd6e8d2e9ac1ab692e31d70a745002d319fbbdc0
Jessica <github email address>
2020-06-15 14:15:05 -0400

    updated controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -33,6 +33,6 @@ class CartItemsController < ApplicationController
     private
 
     def cart_items_params
-        params.require(:cartitems).permit(:quantity, :item_id, :user_id)
+        params.require(:cart_item).permit(:quantity, :item_id, :users_cart_id)
     end
 end
